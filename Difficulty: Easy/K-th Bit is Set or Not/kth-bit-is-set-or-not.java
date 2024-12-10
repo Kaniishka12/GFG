9271b5a6900ffba63418061d6beaad964d6be985
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2024-12-10 23:27:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/K-th Bit is Set or Not/kth-bit-is-set-or-not.java
+++ Difficulty: Easy/K-th Bit is Set or Not/kth-bit-is-set-or-not.java
@@ -11,11 +11,10 @@ import java.util.*;
 class CheckBit {
     // Function to check if Kth bit is set or not.
     static boolean checkKthBit(int n, int k) {
-        // Your code here
-        if((n&(1<<k))==0){
-            return false;
+        if((n&(1<<k))!=0){
+            return true;
         }
-        return true;
+        return false;
     }
 }
 
