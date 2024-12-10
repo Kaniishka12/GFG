b0edbb89e666e297fd049101316cd24dc3bd2da6
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2024-12-10 23:35:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bit Manipulation/bit-manipulation.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int i = sc.nextInt();
+
+            Solution obj = new Solution();
+            obj.bitManipulation(n, i);
+            System.out.println();
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static void bitManipulation(int num, int i) {
+        // code here
+        
+         i = i-1;
+        int ithBit = (num>>i)&1;
+        int numWithSetBit =( num | (1<<i));
+        int clearIthBit = (num & ~(1<<i));
+        System.out.print(ithBit+" "+numWithSetBit+" "+clearIthBit);
+    }
+}
