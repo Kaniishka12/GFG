7af3bb87936961a641dbc23fe572267128405b79
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2024-12-20 21:30:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -33,23 +33,23 @@ class Solution {
     // Function to sort an array of 0s, 1s, and 2s
     public void sort012(int[] arr) {
         // code here
-        int low = 0;   // Pointer for 0
-        int mid = 0;   // Pointer for current element
-        int high = arr.length - 1; // Pointer for 2
+         int low = 0;  
+        int mid = 0;   
+        int high = arr.length - 1; 
         
         while (mid <= high) {
             if (arr[mid] == 0) {
-                // Swap nums[mid] and nums[low], then increment low and mid
+            
                 int temp = arr[low];
                 arr[low] = arr[mid];
                 arr[mid] = temp;
                 low++;
                 mid++;
             } else if (arr[mid] == 1) {
-                // Leave 1 in place and just move mid forward
+                
                 mid++;
             } else {
-                // Swap nums[mid] and nums[high], then decrement high
+            
                 int temp = arr[mid];
                 arr[mid] = arr[high];
                 arr[high] = temp;
