d7ed72c7817e8ecc2e0a194c61a961df6727de48
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-01-07 20:04:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driver {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String arr1[] = br.readLine().split(" ");
+            int[] a = new int[arr1.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(arr1[i]);
+
+            String arr2[] = br.readLine().split(" ");
+            int[] b = new int[arr2.length];
+
+            for (int i = 0; i < arr2.length; i++) b[i] = Integer.parseInt(arr2[i]);
+
+            if (b.length == 1 && b[0] == 0) {
+                b = new int[0];
+            }
+            double res = new Solution().medianOf2(a, b);
+
+            if (res == (int)res)
+                System.out.println((int)res);
+            else
+                System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public double medianOf2(int a[], int b[]) {
+        // Your Code Here
+        
+        // Write your code here.
+        int n1 = a.length, n2 = b.length;
+        //if n1 is bigger swap the arrays:
+        if (n1 > n2) return medianOf2(b, a);
+
+        int n = n1 + n2; //total length
+        int left = (n1 + n2 + 1) / 2; //length of left half
+        //apply binary search:
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            //calculate l1, l2, r1 and r2;
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return Math.max(l1, l2);
+                else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
+        }
+        return 0;
+    }
+    
+}
