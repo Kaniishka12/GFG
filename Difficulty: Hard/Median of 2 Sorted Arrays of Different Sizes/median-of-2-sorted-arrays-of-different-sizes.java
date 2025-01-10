ba9f12f1b12d8e188d37ba5c9a1fc7c9841e59e9
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-01-10 09:58:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
@@ -40,33 +40,70 @@ class Driver {
 
 class Solution {
     public double medianOf2(int a[], int b[]) {
-        // Your Code Here
-        
-        // Write your code here.
-        int n1 = a.length, n2 = b.length;
-        //if n1 is bigger swap the arrays:
-        if (n1 > n2) return medianOf2(b, a);
+      int n1 = a.length, n2 = b.length;
 
-        int n = n1 + n2; //total length
-        int left = (n1 + n2 + 1) / 2; //length of left half
-        //apply binary search:
-        int low = 0, high = n1;
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
-            //calculate l1, l2, r1 and r2;
-            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
-            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
-            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+    // Ensure the first array is the smaller one.
+    if (n1 > n2) {
+        int[] temp = a;
+        a = b;
+        b = temp;
+        n1 = a.length;
+        n2 = b.length;
+    }
 
-            if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 1) return Math.max(l1, l2);
-                else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
-            } else if (l1 > r2) high = mid1 - 1;
-            else low = mid1 + 1;
+    int n = n1 + n2; // Total length of combined arrays
+    int left = (n + 1) / 2; // Size of the left partition
+
+    int low = 0, high = n1;
+
+    // Binary search to find the correct partition.
+    while (low <= high) {
+        int mid1 = (low + high) / 2;
+        int mid2 = left - mid1;
+
+        // Values on either side of the partition.
+        int l1;
+        if (mid1 > 0) {
+            l1 = a[mid1 - 1];
+        } else {
+            l1 = Integer.MIN_VALUE;
         }
-        return 0;
+
+        int l2;
+        if (mid2 > 0) {
+            l2 = b[mid2 - 1];
+        } else {
+            l2 = Integer.MIN_VALUE;
+        }
+
+        int r1;
+        if (mid1 < n1) {
+            r1 = a[mid1];
+        } else {
+            r1 = Integer.MAX_VALUE;
+        }
+
+        int r2;
+        if (mid2 < n2) {
+            r2 = b[mid2];
+        } else {
+            r2 = Integer.MAX_VALUE;
+        }
+
+        if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 1) {
+                return Math.max(l1, l2);
+            } else {
+                return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            }
+        } else if (l1 > r2) {
+            high = mid1 - 1;
+        } else {
+            low = mid1 + 1;
+        }
+    }
+
+    return 0.0;
     }
     
 }
