0f5fa107ab0c96e8ddd920800cc0df0d58b2c8c4
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-07-20 10:02:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,35 +1,9 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().majorityElement(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    static int majorityElement(int arr[]) {
+    int majorityElement(int arr[]) {
         // code here
-        int n=arr.length;
+        int count =0;
         int ele=0;
-        int count=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<arr.length;i++){
             if(count==0){
                 ele=arr[i];
                 count=1;
@@ -42,15 +16,14 @@ class Solution {
             }
         }
         int check=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<arr.length;i++){
             if(arr[i]==ele){
                 check++;
             }
         }
-        
-    if(check>n/2){
-        return ele;
-    }
-    return -1;
+        if(check>(arr.length/2)){
+            return ele;
+        }
+        return -1;
     }
 }
