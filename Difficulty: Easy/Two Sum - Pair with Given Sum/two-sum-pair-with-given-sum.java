21baac9adaf89338f8437e5e1145a4c71657938c
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-07-23 10:43:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
@@ -1,49 +1,13 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-        while (tc-- > 0) {
-            String[] inputLine;
-            inputLine = br.readLine().trim().split(" ");
-            // int n = Integer.parseInt(inputLine[0]);
-            int x = Integer.parseInt(inputLine[0]);
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            int[] arr = new int[tokens.length];
-            for (int i = 0; i < tokens.length; i++) {
-                arr[i] = Integer.parseInt(tokens[i]);
-            }
-
-            boolean ans = new Solution().twoSum(arr, x);
-            System.out.println(ans ? "true" : "false");
-            // System.out.println("~");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     boolean twoSum(int arr[], int target) {
         // code here
-        HashMap<Integer,Integer> hp=new HashMap<>();
+        HashMap<Integer,Integer> hm=new HashMap<>();
         for(int i=0;i<arr.length;i++){
-            if(hp.containsKey(target-arr[i])){
+            int comp=target-arr[i];
+            if(hm.containsKey(comp)){
                 return true;
             }
-            hp.put(arr[i],i);
+            hm.put(arr[i],i);
         }
         return false;
     }
